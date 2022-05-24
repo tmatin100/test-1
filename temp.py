b5109c299f4cf7593c6564b203508de2333898a6
gvelard2 <gabrielvelarde10@gmail.com>
2022-05-24 04:18:28 -0700

    Update 1.1
    Includes figure

--- temp.py
+++ temp.py
@@ -1,2 +1,11 @@
 print("hello world")
 x=5
+y=3
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy import random
+
+fig, ax=plt.subplots()
+x=np.linspace(0,100,100)
+y=random.randint(100, size=(100))
+ax.plot(x,y)
